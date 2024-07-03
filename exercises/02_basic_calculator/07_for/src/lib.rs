f63573d22b9f347f32a872dcc049c6d628601b51
cristotodev <cristom.estevez@gmail.com>
2024-07-03 13:58:45 +0100

    for complete

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,11 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut total = 1;
+    for x in (1..n+1).rev() {
+        println!("{x}");
+        total *= x;
+    }
+    total
 }
 
 #[cfg(test)]
